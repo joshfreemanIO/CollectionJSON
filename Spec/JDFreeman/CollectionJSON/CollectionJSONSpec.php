41668c67ecbbe6f5536cc58c7c9591e18158bbf8
Josh Freeman <jdfreeman11@gmail.com>
2014-08-15 18:18:50 -0500

    Add initital CollectionJSON

---
+++ Spec/JDFreeman/CollectionJSON/CollectionJSONSpec.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of the JDFreeman/CollectionJSON specification library
+ *
+ * @author     Josh Freeman <jdfreeman11@gmail.com>
+ * @copyright  2014 Josh Freeman
+ * @license    MIT
+ * @package    CollectionJSON
+ */
+
+namespace Spec\JDFreeman\CollectionJSON;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class CollectionJSONSpec extends ObjectBehavior
+{
+    /**
+     * Setup the specification
+     *
+     * @return void
+     */
+    public function let()
+    {
+
+    }
+
+    /**
+     * Tear down the specification
+     *
+     * @return void
+     */
+    public function letUp()
+    {
+
+    }
+
+    /**
+     * Test that the class exists
+     *
+     * This spec is auto-generated
+     *
+     * @return void
+     */
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType('JDFreeman\CollectionJSON\CollectionJSON');
+    }
+}
---
+++ src/JDFreeman/CollectionJSON/CollectionJSON.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * This file is part of the JDFreeman/CollectionJSON library
+ *
+ * @author     Josh Freeman <jdfreeman11@gmail.com>
+ * @copyright  2014 Josh Freeman
+ * @license    MIT
+ * @package    CollectionJSON
+ */
+
+namespace JDFreeman\CollectionJSON;
+
+class CollectionJSON
+{
+}
